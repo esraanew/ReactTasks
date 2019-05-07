4f397c3c016f75903a5ffcaa32d3b8ecb20b6c66
esraanew <esraasamir225@gmail.com>
2019-05-07 16:52:47 -0700

    Tasks Of Day1 & Day2

---
+++ src/Components/movieData.js
@@ -0,0 +1,82 @@
+export default [
+    {
+      Title: 'The Extraordinary of Adèle Blanc-Sec',
+      Year: '2010',
+      imdbID: 'tt1179025',
+      Type: 'movie',
+      Watched: true,
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BZjFkOTkyYzMtNWM1My00OGY3LTlhZmYtM2I0ZDNmMjVhY2RhXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg',
+    },
+    {
+      Title: 'SEC Storied',
+      Year: '2011–',
+      imdbID: 'tt6364492',
+      Type: 'series',
+      Watched: true,
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BNGQyYjhkNDUtNmUxNC00Yjc4LWE0ZGUtMjI1YjI0MTQxMjQwL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNDAxNDE0MzY@._V1_SX300.jpg',
+    },
+    {
+      Title: 'Cinq à sec',
+      Year: '1995',
+      imdbID: 'tt0355330',
+      Type: 'movie',
+      Poster: 'N/A',
+    },
+    {
+      Title: 'SEC Ready',
+      Year: '2014',
+      imdbID: 'tt4601536',
+      Type: 'movie',
+      Poster:
+        'https://images-na.ssl-images-amazon.com/images/M/MV5BMTg5Mzg4Mjg4MV5BMl5BanBnXkFtZTgwMDAwMjczNTE@._V1_SX300.jpg',
+    },
+    {
+      Title: 'En zinc sec',
+      Year: '1931',
+      imdbID: 'tt4718048',
+      Watched: true,
+      Type: 'movie',
+      Poster: 'N/A',
+    },
+    {
+      Title: 'Martini sec',
+      Year: '1933',
+      imdbID: 'tt4844972',
+      Type: 'movie',
+      Poster: 'N/A',
+    },
+    {
+      Title: 'Noisy-le-Sec, le laboratoire de la reconstruction',
+      Year: '1948',
+      imdbID: 'tt4987562',
+      Watched: true,
+      Type: 'movie',
+      Poster: 'N/A',
+    },
+    {
+      Title: 'SEC Storied',
+      Year: '2015',
+      imdbID: 'tt5324708',
+      Type: 'movie',
+      Poster: 'N/A',
+    },
+    {
+      Title: 'Trou noir et blanc sec',
+      Year: '1992',
+      imdbID: 'tt5474606',
+      Type: 'movie',
+      Poster:
+        'https://www.recoveryfirst.org/wp-content/uploads/2013/08/bigstock-Movie-Reel-4145975-300x297.jpg',
+    },
+    {
+      Title: 'SEC Inside',
+      Year: '2015–',
+      imdbID: 'tt5884190',
+      Type: 'series',
+      Poster:
+        'https://images-na.ssl-images-amazon.com/images/M/MV5BYmY2OGYxM2QtYjQxZC00ZDZlLWFkNGEtOTFmNGRkZDkwNjY1XkEyXkFqcGdeQXVyNTg0MTczNjM@._V1_SX300.jpg',
+    },
+  ];
+  
